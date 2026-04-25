c49de4f6314f10e9cf4cd46a035ffc0cf7ce5036
Coko <91132775+Coko7@users.noreply.github.com>
2026-04-25 23:14:24 +0200

    feat: improve fleet.sh script with new actions

--- scripts/fleet.sh
+++ scripts/fleet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env boat
 
-actions='task:;start a new activity or resume an existing one
+actions='resume:;resume an existing activity
+new:;start a new activity
 stop:;pause/stop the current activity
 cancel:;cancel the current activity
 meeting:;start a new meeting from a list of presets
@@ -29,7 +30,10 @@ meeting)
   meeting:misc)
     now=$(date +"%Y-%m-%d %H:%M")
     def_meeting_name="unnamed meeting (at $now)"
-    meeting_name=$(gum input --placeholder="$def_meeting_name" --prompt="Meeting name> ")
+    if ! meeting_name=$(gum input --placeholder="$def_meeting_name" --prompt="Meeting name> "); then
+      exit 1
+    fi
+
     if [ -z "$meeting_name" ]; then
       meeting_name=$def_meeting_name
     fi
@@ -39,10 +43,56 @@ meeting)
     exit 0
     ;;
   esac
+
   meet_id=$(boat report --filter-by-tags "$meeting_tag" --period 'all-time' --json | jq --raw-output '.[0].id')
   echo "$meet_id" | less
   ;;
+
+new)
+  if ! act_name=$(gum input --placeholder="work on boat-cli" --prompt="Activity name> "); then
+    exit 1
+  fi
+
+  [[ -z "$act_name" ]] && exit 1
+
+  act_id=$(boat new "$act_name" --tags task:misc --start-now --json | jq '.id')
+  ntfy-toast.sh 'boat-fleet' "Started new task: $act_name ($act_id)"
+  exit 0
+  ;;
+
+cancel)
+  current=$(boat get --json)
+  act_id=$(jq '.activity.id' <<<"$current")
+  act_name=$(jq --raw-output '.activity.name' <<<"$current")
+  boat cancel
+  ntfy-toast.sh 'boat-fleet' "Cancelled activity: $act_name ($act_id)"
+  ;;
+
+resume)
+  activities=$(boat report --period=all-time --json)
+  act_id=$(jq --raw-output '.[] | "\(.id): \(.name)"' <<<"$activities" | sort --numeric-sort --reverse | fzf-rofi.sh \
+    --delimiter=':' --nth=1 --accept-nth=1 --input-label ' Input ' \
+    --list-label ' Recent Activities ' --preview="figlet {1} | lolcat --force")
+
+  [[ -z "$act_id" ]] && exit 1
+
+  boat start "$act_id"
+  current=$(boat get --json)
+  act_name=$(jq --raw-output '.activity.name' <<<"$current")
+  ntfy-toast.sh 'boat-fleet' "Resumed activity: $act_name ($act_id)"
+
+  ;;
+
+edit)
+  boat edit --period=today
+  ;;
+stop)
+  activity=$(boat get --json | jq '.activity')
+  act_id=$(jq '.id' <<<"$activity")
+  act_name=$(jq --raw-output '.name' <<<"$activity")
+
+  boat stop && ntfy-toast.sh 'boat-fleet' "Stopped tracking activity: $act_name ($act_id)"
+  ;;
 *)
-  echo "$pick" | less
   ;;
 esac
