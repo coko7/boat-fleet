3a77b0d3fe911d12fd83402de619129545857909
Coko <91132775+Coko7@users.noreply.github.com>
2026-04-27 13:04:19 +0200

    feat: improve fleet script + docs

--- README.md
+++ README.md
@@ -4,16 +4,26 @@ A collection of shell scripts to augment the [`boat-cli`](https://github.com/cok
 
 ## ⚠️ Requirements
 
-Here are all the tools and scripts you need to have in your `$PATH` to be able to make full use of the **boat fleet scripts**.
+Here are all the programs/scripts you need to have in your `$PATH` to be able to make full use of the **boat fleet scripts**.
 
-**CLI tools:**
+**Core dependencies:**
 
 - [boat](https://github.com/coko7/boat-cli): ⛵ The core activity tracking library
 - [fzf](https://github.com/junegunn/fzf): 🌸 A command-line fuzzy finder
 - [jq](https://github.com/jqlang/jq): ✨ A command-line JSON processor
 - [gum](https://github.com/charmbracelet/gum): 🎀 A tool for glamorous shell scripts
 
+**Hyprland-specific dependencies:**
+
+- [hyprctl](https://wiki.hypr.land/Configuring/Advanced-and-Cool/Using-hyprctl): [Hyprland](https://hypr.land) dispatcher, used to launch floating [Neovide](https://neovide.dev) instances for activity notes
+
+**Styling dependencies:**
+
+- [figlet](https://www.figlet.org/): ⬆️ used for big ASCII art text in the preview pane
+- [lolcat](https://github.com/busyloop/lolcat): 🌈 used for colorful output in the preview pane and notifications
+
 **Companion scripts:**
 
-- [fzf-rofi.sh](https://github.com/coko7/.dotfiles/blob/main/.config/local/bin-sh/global/fzf-rofi.sh): my tiny wrapper around fzf to use it in a floating terminal pane
+- [floating-neovide.sh](https://github.com/coko7/.dotfiles/blob/main/.config/local/bin-sh/global/floating-neovide.sh): a custom script to launch a floating [Neovide](https://neovide.dev) instance with a specific note file, used for activity notes
+- [fzf-rofi.sh](https://github.com/coko7/.dotfiles/blob/main/.config/local/bin-sh/global/fzf-rofi.sh): a custom wrapper around fzf to use it with [rofi](https://github.com/davatorium/rofi), used for all interactive selection in this script
 - [ntfy-toast.sh](https://github.com/coko7/.dotfiles/blob/main/.config/local/bin-sh/global/ntfy-toast.sh): a simple wrapper around [`notify-send`](https://man.archlinux.org/man/notify-send.1.en)
--- scripts/fleet.sh
+++ scripts/fleet.sh
@@ -1,7 +1,49 @@
 #!/usr/bin/env boat
 
-actions='resume:;resume an existing activity
-new:;start a new activity
+function ensure_installed() {
+  if ! command -v "$1" &>/dev/null; then
+    echo "This script requires $1 to be installed."
+    exit 1
+  fi
+}
+
+###################################################################
+#                                                                 #
+#  ____                  _                               _        #
+# |  _ \ ___  __ _ _   _(_)_ __ ___ _ __ ___   ___ _ __ | |_ ___  #
+# | |_) / _ \/ _` | | | | | '__/ _ \ '_ ` _ \ / _ \ '_ \| __/ __| #
+# |  _ <  __/ (_| | |_| | | | |  __/ | | | | |  __/ | | | |_\__ \ #
+# |_| \_\___|\__, |\__,_|_|_|  \___|_| |_| |_|\___|_| |_|\__|___/ #
+#               |_|                                               #
+#                                                                 #
+###################################################################
+
+# Core
+ensure_installed boat # the boat-cli itself
+ensure_installed fzf  # a CLI fuzzy finder, used for interactive selection of activities and meetings
+ensure_installed jq   # for parsing JSON output from boat-cli
+ensure_installed gum  # for interactive input prompts, used when creating new activities or meetings
+
+# Hyprland-specific
+ensure_installed hyprctl # Hyprland dispatcher, used to launch floating neovide instances for activity notes
+
+# Styling only
+ensure_installed figlet # used for big ASCII art text in the preview pane
+ensure_installed lolcat # used for colorful output in the preview pane and notifications
+
+# custom scripts
+ensure_installed floating-neovide.sh # a custom script to launch a floating neovide instance with a specific note file, used for activity notes
+ensure_installed fzf-rofi.sh         # a custom wrapper around fzf to use it with rofi, used for all interactive selection in this script
+ensure_installed ntfy-toast.sh       # a custom script to send notifications via ntfy, used for activity start/stop/cancel notifications
+
+# Global declarations
+
+NTFY_TOPIC='ntfy-boat-fleet'
+TASK_NOTES_DIR="$HOME/Documents/boat-acts"
+
+actions='new:;start a new activity
+note:;view/take notes for the current activity
+resume:;resume an existing activity
 stop:;pause/stop the current activity
 cancel:;cancel the current activity
 meeting:;start a new meeting from a list of presets
@@ -11,12 +53,22 @@ meeting_presets='Daily meeting:daily
 Weekly meeting:weekly
 Miscellaneous meeting:misc'
 
+# Actual script logic
+
 pick=$(column --separator=';' --table <<<"$actions" | fzf-rofi.sh \
   --delimiter=':' --nth=1 --accept-nth=1 \
   --border-label ' Boat Fleet Actions ' --input-label ' Input ' \
   --list-label ' Actions ' --preview="figlet {1} | lolcat --force")
 
 [[ -z "$pick" ]] && exit 1
+
+function notify_info() {
+  ntfy-toast.sh 'boat-fleet' "ℹ️ $1" boat-cli-icon "$NTFY_TOPIC" /usr/share/sounds/freedesktop/stereo/bell.oga
+}
+
+function notify_error() {
+  ntfy-toast.sh 'boat-fleet' "⚠️ $1" boat-cli-icon "$NTFY_TOPIC" "$HOME/Music/sounds/nope.wav"
+}
 
 case "$pick" in
 meeting)
@@ -39,16 +91,35 @@ meeting)
     fi
 
     act_id=$(boat new "$meeting_name" --tags meeting:misc --start-now --json | jq '.id')
-    ntfy-toast.sh 'boat-fleet' "Started meeting: $meeting_name ($act_id)"
+    notify_info "Started meeting: $meeting_name ($act_id)"
     exit 0
     ;;
   esac
 
   meet_id=$(boat report --filter-by-tags "$meeting_tag" --period 'all-time' --json | jq --raw-output '.[0].id')
-  echo "$meet_id" | less
+  # echo "$meet_id" | less
+  ;;
+note)
+  if ! current=$(boat get --json); then
+    notify_error "No current activity"
+    exit 1
+  fi
+
+  act_id=$(jq '.activity.id' <<<"$current")
+  act_name=$(jq --raw-output '.activity.name' <<<"$current")
+
+  note_path="$TASK_NOTES_DIR/note_${act_id}.md"
+  if [ ! -f "$note_path" ]; then
+    notify_info "No existing notes for current activity. Creating new note at $note_path"
+    echo "# ${act_name} - Notes" >"$note_path"
+  fi
+
+  wm_class_prefix="boat-view-note-$act_id"
+  hyprctl dispatch exec "floating-neovide.sh $wm_class_prefix $note_path"
   ;;
 
 new)
+  echo " New Activity" | figlet -f slant | lolcat --force --seed 42
   if ! act_name=$(gum input --placeholder="work on boat-cli" --prompt="Activity name> "); then
     exit 1
   fi
@@ -56,30 +127,36 @@ new)
   [[ -z "$act_name" ]] && exit 1
 
   act_id=$(boat new "$act_name" --tags task:misc --start-now --json | jq '.id')
-  ntfy-toast.sh 'boat-fleet' "Started new task: $act_name ($act_id)"
+  notify_info "Started new activity: $act_name ($act_id)"
   exit 0
   ;;
 
 cancel)
-  current=$(boat get --json)
+  if ! current=$(boat get --json); then
+    notify_error "No current activity"
+    exit 1
+  fi
+
   act_id=$(jq '.activity.id' <<<"$current")
   act_name=$(jq --raw-output '.activity.name' <<<"$current")
   boat cancel
-  ntfy-toast.sh 'boat-fleet' "Cancelled activity: $act_name ($act_id)"
+  notify_info "Canceled activity: $act_name ($act_id)"
   ;;
 
 resume)
   activities=$(boat report --period=all-time --json)
-  act_id=$(jq --raw-output '.[] | "\(.id): \(.name)"' <<<"$activities" | sort --numeric-sort --reverse | fzf-rofi.sh \
-    --delimiter=':' --nth=1 --accept-nth=1 --input-label ' Input ' \
-    --list-label ' Recent Activities ' --preview="figlet {1} | lolcat --force")
+  act_id=$(jq --raw-output '.[] | "\(.id): \(.name)"' <<<"$activities" |
+    sort --numeric-sort --reverse |
+    fzf-rofi.sh \
+      --delimiter=':' --nth=1 --accept-nth=1 --input-label ' Input ' \
+      --list-label ' Recent Activities ' --preview="figlet {1} | lolcat --force")
 
   [[ -z "$act_id" ]] && exit 1
 
   boat start "$act_id"
   current=$(boat get --json)
   act_name=$(jq --raw-output '.activity.name' <<<"$current")
-  ntfy-toast.sh 'boat-fleet' "Resumed activity: $act_name ($act_id)"
+  notify_info "Resumed activity: $act_name ($act_id)"
 
   ;;
 
@@ -91,7 +168,7 @@ stop)
   act_id=$(jq '.id' <<<"$activity")
   act_name=$(jq --raw-output '.name' <<<"$activity")
 
-  boat stop && ntfy-toast.sh 'boat-fleet' "Stopped tracking activity: $act_name ($act_id)"
+  boat stop && notify_info "Stopping activity: $act_name ($act_id)"
   ;;
 *)
   ;;
